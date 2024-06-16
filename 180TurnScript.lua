2bd07d12e3ea539f5e8eede3caee8c4ada963fcf
Esmaabi <60576985+KristjanPikhof@users.noreply.github.com>
2024-06-16 20:17:26 +0300

    Added option to set degrees

--- 180TurnScript.lua
+++ 180TurnScript.lua
@@ -2,10 +2,15 @@
 -- This number varies based on game settings, your mouse's DPI, and your in-game sensitivity.
 -- For instance, with a 1080p screen at 1440x1080 resolution, 1600 DPI and 0.32 sensitivity,
 -- the correct distance is 25568 pixels. Adjust this number to fit your setup.
-local distance_to_turn_180 = 25568
+-- You can set your desired turn in degrees, and the script will adjust the pixels accordingly.
+local pixels_for_180 = 25568 -- Adjust this for a perfect 180 turn based on your DPI and sensitivity.
+local desired_degrees = 180 -- Set your desired degrees turn 0-270 degrees.
+
+-- Calculate the proportional distance for the desired turn
+local pixels_to_turn = (pixels_for_180 / 180) * desired_degrees
 
 function OnEvent(event, arg)
     if event == "MOUSE_BUTTON_PRESSED" and arg == 5 then
-        MoveMouseRelative(distance_to_turn_180, 0)
+        MoveMouseRelative(pixels_to_turn, 0)
     end
 end
